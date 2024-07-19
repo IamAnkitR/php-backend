1cec56d3539d43919118ebe5758018962b84d11b
IamAnkitR <ardb8755@gmail.com>
2024-07-19 22:30:56 +0530

    feat: index added

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+  echo 'Hello';
+
+  // echo :: Outputs strings, numbers, html, etc
+
+  // print :: Works like echo, but can only tskr in a single argument
+
+  print 123;
+
+  // print_r() - Print single values and arrays
+  print_r([1,2,3]);
+
+  // var_dump() :: Returns more info like data type and length
+ var_dump(true);
+
+  // var_export() :: Outputs a string representation of a variable
+  var_export('Hello');
+?>  
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php echo 'Hello'?></h1>
+</body>
+</html>
+
